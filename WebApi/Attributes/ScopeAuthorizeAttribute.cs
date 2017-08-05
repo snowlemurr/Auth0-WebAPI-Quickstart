e9f23d20c7857f4bd48fa2180ed50462ff2a599f
snowlemurr <snowlemurr@gmail.com>
2017-08-05 12:52:55 -0400

    Adding files

---
+++ WebApi/Attributes/ScopeAuthorizeAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace WebApi.Attributes
+{
+    public class ScopeAuthorizeAttribute : AuthorizeAttribute
+    {
+        private readonly string scope;
+
+        public ScopeAuthorizeAttribute(string scope)
+        {
+            this.scope = scope;
+        }
+
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            base.OnAuthorization(actionContext);
+
+            ClaimsPrincipal principal = actionContext.ControllerContext.RequestContext.Principal as ClaimsPrincipal;
+            if (principal != null && principal.HasClaim(c => c.Type == "scope"))
+            {
+                var scopes = principal.Claims.FirstOrDefault(c => c.Type == "scope").Value.Split(' ');
+
+                if (!scopes.Any(s => s == scope))
+                {
+                    HandleUnauthorizedRequest(actionContext);
+                }
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Attributes;
+
+namespace WebApi.Controllers
+{
+    public class Message
+    {
+        public DateTime time { get; set; }
+        public string subject { get; set; }
+    }
+
+    [RoutePrefix("api/messages")]
+    public class MessagesController : ApiController
+    {
+        [ScopeAuthorize("read:messages")]
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            return Ok(new
+            {
+                Message = "Got all messages"
+            });
+        }
+
+        [ScopeAuthorize("create:messages")]
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult Create(Message message)
+        {
+            return Ok(new
+            {
+                Message = "Created messages"
+            });
+        }
+    }
+}
